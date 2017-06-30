005f0ec5682bb7eba435718a2ea4f9f5267d8757
stormcat24 <stormcat24@stormcat.io>
2017-06-30 16:48:07 +0900

    modify port option

--- README.md
+++ README.md
@@ -1,1 +1,26 @@
 # plasma-mackerel-plugin
+
+This mackerel plugin provides [Plasma](https://github.com/openfresh/plasma) connection metrics.
+
+# Usage
+
+## Install
+
+```bash
+$ go get github.com/openfresh/plasma-mackerel-plugin
+```
+
+## setting
+
+You should locate binary of plasma-mackerel-plugin to any directory.
+
+```
+[plugin.metrics.plasma]
+command = "/path/to/plasma-mackerel-plugin -host=127.0.0.1:9999"
+```
+
+License
+===
+See [LICENSE](LICENSE).
+
+Copyright © CyberAgent, Inc. All Rights Reserved.
--- main.go
+++ main.go
@@ -11,7 +11,7 @@ import (
 
 type PlasmaPlugin struct {
 	Prefix string
-	Host   string
+	Port   int
 }
 
 func (p PlasmaPlugin) GraphDefinition() map[string](mp.Graphs) {
@@ -21,28 +21,28 @@ func (p PlasmaPlugin) GraphDefinition() map[string](mp.Graphs) {
 			Label: labelPrefix,
 			Unit:  "float",
 			Metrics: [](mp.Metrics){
-				mp.Metrics{Name: "connections", Label: "Connections"},
-				mp.Metrics{Name: "connections_sse", Label: "ConnectionsSSE"},
-				mp.Metrics{Name: "connections_grpc", Label: "ConnectionsGRPC"},
+				mp.Metrics{Name: "connections", Label: "Connections All"},
+				mp.Metrics{Name: "connections_sse", Label: "Connections SSE"},
+				mp.Metrics{Name: "connections_grpc", Label: "Connections GRPC"},
 			},
 		},
 	}
 }
 
 func (p PlasmaPlugin) FetchMetrics() (map[string]interface{}, error) {
-	client := metrics.NewPlasmaMetricsClient(p.Host)
+	client := metrics.NewPlasmaMetricsClient(p.Port)
 	return client.GetMetrics()
 }
 
 func main() {
 	optPrefix := flag.String("metric-key-prefix", "plasma", "Metric key prefix")
 	optTempfile := flag.String("tempfile", "", "Temp file name")
-	optHost := flag.String("host", "", "plasma mertics host")
+	optPort := flag.Int("port", 9999, "plasma mertics port")
 	flag.Parse()
 
 	p := PlasmaPlugin{
 		Prefix: *optPrefix,
-		Host:   *optHost,
+		Port:   *optPort,
 	}
 	helper := mp.NewMackerelPlugin(p)
 	helper.Tempfile = *optTempfile
